f1334f7b4abee25c145b3ccc66cee722af1c7ca9
jennifer.chan <jennifer.chan@lalamove.com>
2017-11-14 10:55:28 +0800

    UPT: remove unnecessary lines in reducer

--- Mapreduce/reducer.py
+++ Mapreduce/reducer.py
@@ -7,14 +7,10 @@ import sys
 SCORE_MIN = 1
 SCORE_MAX = 5
 
-def read_mapper_output(mapper_output):
-    #sort result by score
-    for line in (mapper_output):
-        yield line
 
-def reducer(mapper_func, k_max, separator='\t'):
-    data = read_mapper_output(mapper_func(k_max))
-    #data here is sorted by rating and k-length already
+def reducer(mapper_func, k_max):
+    #get mapped result
+    data = mapper_func(k_max)
 
     # sort and group with respect to score, k-length, word
     grouper = itemgetter(0, 1, 2)
@@ -22,4 +18,4 @@ def reducer(mapper_func, k_max, separator='\t'):
     for key, group in groupby(sorted(data, key = grouper), grouper):
         total_count = sum(int(count) for score, k, current_word, count in group)
         #output score, k-length, frequency, k-shingle
-        print (key[0], key[1], total_count, key[2])
+        yield (key[0], key[1], total_count, key[2])
