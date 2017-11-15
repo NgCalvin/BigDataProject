7610b214621d6f8eba61e4225dbdb80c21015380
Ben Lei <ben181231@gmail.com>
2017-11-15 16:53:43 +0800

    Minor update finding frequent item set script

--- Mapreduce/cleanup.py
+++ Mapreduce/cleanup.py
@@ -13,33 +13,38 @@ def cleanup(reduced_result, top_max):
     data = reduced_result
 
     # sort by score asc, k-length asc, frequency desc, alphabetic asc
-    sorter = [(0,False),(1,False),(2,True),(3,False)]
+    sorter = [(0, False), (1, False), (2, True), (3, False)]
 
-    #sort data
+    # sort data
     for (key, rev) in reversed(sorter):
-    	data = sorted(data, key = itemgetter(key), reverse=rev)
+        data = sorted(data, key = itemgetter(key), reverse=rev)
 
     prev_score = SCORE_MIN-1
     prev_k = 1-1
     count = 0
-    
+
     for line in data:
-    	#current score and k-length for this record
-    	score = line[0]
-    	k = line[1]
+        # current score and k-length for this record
+        score = line[0]
+        k = line[1]
 
-    	#if it is beginning of new group i.e. different k-length / score with previous
-    	if (k != prev_k) | (score != prev_score):
-    		count = 0
-    		print "top {} for score = {} and k-length = {}".format(top_max,score,k)
+        # if it is beginning of new group i.e. different k-length / score with previous
+        if (k != prev_k) or (score != prev_score):
+            count = 0
+            print("top {} for score = {} and k-length = {}".format(top_max,
+                                                                   score,
+                                                                   k))
 
-    	#if already outputted top n frequent words in the same group, skip
-    	if count == top_max:
-    		continue
+        # if already outputted top n frequent words in the same group, skip
+        if count == top_max:
+            continue
 
-        #output score, k-length, frequency, k-shingle
-        print "score: {}, k-length: {}, frequency: {}, shingle: {}".format(line[0],line[1],line[2],line[3])
-        
+        # output score, k-length, frequency, k-shingle
+        print("score: {}, ".format(line[0]) +
+            "k-length: {}, ".format(line[1]) +
+            "frequency: {}, ".format(line[2]) +
+            "shingle {}".format(line[3]))
+
         count += 1
         prev_score = score
-    	prev_k = k
+        prev_k = k
--- Mapreduce/main.py
+++ Mapreduce/main.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python
 
-import sys,os
-#change your directory here if there is trouble using relative directory in python interpreter
-sys.path.append(r"/Users/heinam/BigDataProject.git/Mapreduce/")
+import os
+import sys
+
 from mapper import *
 from reducer import *
 from cleanup import *
 
-
-	
-
 if __name__ == '__main__':
-	#change variables here
-	k_max = 5 #define maximum k-length shingle
-	top_max = 10 #define number of top frequent words
-	min_support = 1 #define minimum support
+    # change variables here
+    k_max = 5 # define maximum k-length shingle
+    top_max = 10 # define number of top frequent words
+    min_support = 1 # define minimum support
 
-	#start
-	reduced_result = reducer(mapper,k_max,min_support)
-	cleanup(reduced_result,top_max)
+    # start
+    reduced_result = reducer(mapper,k_max,min_support)
+    cleanup(reduced_result,top_max)
