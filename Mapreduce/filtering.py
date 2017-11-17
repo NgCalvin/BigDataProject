ecd3d7b42c4474d7f762113c562152ee815a88e4
taylortest <33101100+taylortest@users.noreply.github.com>
2017-11-18 04:13:27 +0800

    update filtering
    modified code between to cater the mapreduce job output format:
    <score>\t<k>\t<shingle>\t<count>
    and the output remains the same 
    <score><k><count><shingle> 
    to cope with the prediction coding

--- Mapreduce/filtering.py
+++ Mapreduce/filtering.py
@@ -1,43 +1,59 @@
 #!/usr/bin/env python
 
+
 import sys
 
+import csv
 from itertools import groupby
 from operator import itemgetter
+
 
 SCORE_MIN = 1
 SCORE_MAX = 5
 
-def filter_result(reduced_result, top_max):
+
+def filter_result(file_name, top_max):
     #get mapped result
-    data = reduced_result
+    data = list(csv.reader(open(file_name, 'rb'), delimiter='\t'))
+    #data: score, k-length, k-shingle, count
 
     # sort by score asc, k-length asc, frequency desc, alphabetic asc
-    sorter = [(0, False), (1, False), (2, True), (3, False)]
+    #sorter = [(0, False), (1, False), (2, True), (3, False)]
+    sorter = [(0, False), (1, False), (3, True), (2, False)]
+
+    # convert count into integer for sorting
+    for line in data:
+        line[3] = int(line[3])
 
     # sort data
     for (key, rev) in reversed(sorter):
         data = sorted(data, key = itemgetter(key), reverse=rev)
 
+
     prev_score = SCORE_MIN - 1
     prev_k = 1-1
     count = 0
+
 
     for line in data:
         # current score and k-length for this record
         score = line[0]
         k = line[1]
 
+
         # if it is beginning of new group i.e. different k-length / score with previous
         if (k != prev_k) or (score != prev_score):
             count = 0
+
 
         # if already outputted top n frequent words in the same group, skip
         if count == top_max:
             continue
 
+
         # output score, k-length, frequency, k-shingle
-        yield (line[0], line[1], line[2], line[3])
+        print (line[0], line[1], line[3], line[2])
+
 
         count += 1
         prev_score = score
