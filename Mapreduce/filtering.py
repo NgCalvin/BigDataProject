bb4ab5788d244bc85ac3ee13c8fd868950808dc6
Ben Lei <ben181231@gmail.com>
2017-11-21 02:58:28 +0800

    Update 'filtering.py' to accept execution arguments

--- Mapreduce/filtering.py
+++ Mapreduce/filtering.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-
-
 import sys
 
 import csv
@@ -13,30 +11,27 @@ SCORE_MAX = 5
 
 
 def filter_result(file_name, top_max, file_csv):
-    #get mapped result
-    data = list(csv.reader(open(file_name, 'rb'), delimiter='\t'))
-    #data format: score, k-length, k-shingle, count
-    #change to: score, k-length, count, k-shingle
-    #make count to be integer for sorting
+    # get mapped result
+    data = list(csv.reader(open(file_name, 'r'), delimiter='\t'))
+
+    # data format: score, k-length, k-shingle, count
+    # change to: score, k-length, count, k-shingle
+    # make count to be integer for sorting
     for line in data:
         line[2] , line[3] = line[3] , line[2]
         line[2] = int(line[2])
-    
 
     # sort by score asc, k-length asc, frequency desc, alphabetic asc
     sorter = [(0, False), (1, False), (2, True), (3, False)]
-    #sorter = [(0, False), (1, False), (3, True), (2, False)]
-
 
     # sort data
     for (key, rev) in reversed(sorter):
         data = sorted(data, key = itemgetter(key), reverse=rev)
 
-
     prev_score = SCORE_MIN - 1
-    prev_k = 1-1
+    prev_k = 0
     count = 0
-    
+
     output = []
 
     for line in data:
@@ -56,16 +51,32 @@ def filter_result(file_name, top_max, file_csv):
 
 
         # output score, k-length, frequency, k-shingle
-        #print (line[0], line[1], line[2], line[3])
         output.append([line[0], line[1], line[2], line[3]])
 
         count += 1
         prev_score = score
         prev_k = k
-    
+
     #export to csv
-    with open(file_csv+ '.csv', 'w') as output_csv:
+    with open(file_csv, 'w') as output_csv:
         writer = csv.writer(output_csv, delimiter=',', lineterminator='\n')
         writer.writerow(['score', 'k', 'frequency', 'shingle']) # header
         writer.writerows(output)
-        
+
+if __name__ == '__main__':
+    argc = len(sys.argv)
+    if argc != 4:
+        try:
+            script_name = sys.argv[0]
+        except IndexError:
+            script_name = 'filtering.py'
+        print >> sys.stderr, (
+            'Usage: python {} '.format(script_name) +
+                '<InputFile> <FilterCount> <OutputFile>')
+        exit(1)
+
+    input_file = sys.argv[1]
+    filter_count = int(sys.argv[2])
+    output_file = sys.argv[3]
+
+    filter_result(input_file, filter_count, output_file)
--- Mapreduce/main_filtering.py
+++ Mapreduce/main_filtering.py
@@ -1,24 +1,18 @@
 #!/usr/bin/env python
 
 import sys,os
-#change your directory here if there is trouble using relative directory in python interpreter
-#sys.path.append(r"/Users/heinam/BigDataProject.git/Mapreduce/")
-#from mapper import *
-#from reducer import *
+
 from filtering import *
 
 
 def main():
-    k_max = 5 #define maximum k-length shingle
-    top_max = 10 #define number of top frequent words
-    #reducer(mapper,k_max)
-    
-    # filter MapReduce output and output as csv
-    # filter_result(MapReduce_result, top_max, output_file_csv)
-    filtered_folder = 'filtered'
-    filter_result('part-00000_trial2', top_max, filtered_folder)
-    # output: score, k-length, frequency, k-shingle 
-    
+    k_max = 5 # define maximum k-length shingle
+    top_max = 10 # define number of top frequent words
+    input_file = 'part-00000_trial2'
+    output_file = 'filtered.csv'
+
+    filter_result(input_file, top_max, output_file)
+
     '''
     # PREDICTION
     predict_file = 'processed_test'
